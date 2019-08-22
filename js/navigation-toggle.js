1ab9fb26bf01e3d54ce5dfe79ce8eaf846e768ee
meranicosme <merani@heysparkbox.com>
2019-08-22 13:38:21 -0400

    fix: js navigation for removing class and adding toggle

--- js/navigation-toggle.js
+++ js/navigation-toggle.js
@@ -1,13 +1,23 @@
-const buttonTags = document.querySelectorAll('.c-navigation__list-item');
+const buttonTags = document.querySelectorAll('.c-navigation__list-item > button');
+const navListItems = [...buttonTags].map(function(buttonTag) {
+  return buttonTag.parentElement;
+});
+
 let arrowTags = document.querySelectorAll('.c-navigation__arrow');
 console.log(buttonTags);
 
 
 
-buttonTags.forEach(function(buttonTag) {
-  buttonTag.addEventListener('click', function(event){
-event.currentTarget.classList.toggle('c-navigation__list-item--is-active');
-event.currentTarget.querySelector('.c-navigation__arrow').classList.toggle('c-navigation__arrow--is-active');
-
+navListItems.forEach(function(navListItem1) {
+  navListItem1.addEventListener('click', function(event) {
+    let buttonClicked = event.currentTarget;
+    navListItems.forEach(function(navListItem2) {
+      if (navListItem2 !== buttonClicked) {
+        navListItem2.classList.remove('c-navigation__list-item--is-active');
+        navListItem2.querySelector('.c-navigation__arrow').classList.remove('c-navigation__arrow--is-active');
+      }
+    });
+    buttonClicked.classList.toggle('c-navigation__list-item--is-active');
+    buttonClicked.querySelector('.c-navigation__arrow').classList.toggle('c-navigation__arrow--is-active');
   });
 });
